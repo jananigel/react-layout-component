970fd55b655be4f48f66aa856bbc0bab62b14022
jananigel <jananigel@hotmail.com>
2021-01-28 01:28:13 +0800

    fixed: invalid class name

--- src/layouts/three-rows/three-rows-layout.js
+++ src/layouts/three-rows/three-rows-layout.js
@@ -1,23 +1,17 @@
 import React from "react";
 import "./three-rows-layout.scss";
 
-export const ThreeRowsLayout = ({header, content, footer}) => {
-  console.log('header = ', header, ' contnt = ', content, ' footer = ', footer);
+export const ThreeRowsLayout = ({ header, content, footer }) => {
+  console.log("header = ", header, " contnt = ", content, " footer = ", footer);
   return (
-  <>
-    <div class="main">
-      <div className="header flex">
-        {header}
+    <>
+      <div class="main">
+        <div className="header flex">{header}</div>
+        <div className="container flex-center">{content}</div>
+        <div className="footer flex-center">{footer}</div>
       </div>
-      <div class="container flex-center">
-        {content}
-      </div>
-      <div class="footer flex-center">
-        {footer}
-      </div>
-    </div>
-  </>
-  )
-}
+    </>
+  );
+};
 
-export default ThreeRowsLayout
+export default ThreeRowsLayout;
