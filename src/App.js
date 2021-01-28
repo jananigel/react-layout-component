12a6da76b56d8a026b2f31af060638f9fda2f231
jananigel <jananigel@hotmail.com>
2021-01-28 23:43:08 +0800

    feat: implement clear function and input change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import ThreeRowsLayout from "./layouts/three-rows/three-rows-layout.js";
 
@@ -15,39 +15,78 @@ const Header = () => {
   );
 };
 
-const Content = () => {
+const Content = ({ leftValue, rightValue, inputChange }) => {
+  const leftInputChangedHandler = event => {
+    inputChange({ left: event.target.value });
+  };
+
+  const rightInputChangeHandler = event => {
+    inputChange({ right: event.target.value });
+  };
   return (
     <>
       <div className="left">
-        <input />
+        <input value={leftValue} onChange={leftInputChangedHandler} />
         <span>GB</span>
       </div>
       <div className="center">
-        <div className="label"> = </div>
+        <div className="label">
+          {" "}
+          {`<`}={`>`}{" "}
+        </div>
       </div>
       <div className="right">
-        <input />
+        <input
+          value={rightValue}
+          onChange={event => inputChange({ right: event.target.value })}
+        />
         <span>MB</span>
       </div>
     </>
   );
 };
 
-const Footer = () => {
+const Footer = ({ clearClick }) => {
+  const onBtnClick = e => {
+    clearClick(e);
+  };
+
   return (
     <>
-      <button>Clear</button>
+      <button onClick={onBtnClick}>Clear</button>
     </>
   );
 };
 
 export const App = () => {
+  const [inputValue, setInputValue] = useState({
+    leftValue: "",
+    rightValue: ""
+  });
+  const onInputChange = e => {
+    setInputValue({
+      leftValue: e.left,
+      rightValue: e.right
+    });
+  };
+  const onClearClick = e => {
+    setInputValue({
+      leftValue: "",
+      rightValue: ""
+    });
+  };
   return (
     <>
       <ThreeRowsLayout
         header={<Header />}
-        content={<Content />}
-        footer={<Footer />}
+        content={
+          <Content
+            leftValue={inputValue["leftValue"]}
+            rightValue={inputValue["rightValue"]}
+            inputChange={onInputChange}
+          />
+        }
+        footer={<Footer clearClick={onClearClick} />}
       />
     </>
   );
--- src/layouts/three-rows/three-rows-layout.js
+++ src/layouts/three-rows/three-rows-layout.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./three-rows-layout.scss";
 
 export const ThreeRowsLayout = ({ header, content, footer }) => {
-  console.log("header = ", header, " contnt = ", content, " footer = ", footer);
   return (
     <>
-      <div class="main">
+      <div className="main">
         <div className="header flex">{header}</div>
         <div className="container flex-center">{content}</div>
         <div className="footer flex-center">{footer}</div>
